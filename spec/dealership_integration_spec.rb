15e8ec4ececd602fe1ae0fb00063d1be2ee64b9a
Anna Horodetska and David Shin <anna@>
2017-04-26 10:55:13 -0700

    update dealership_integration_spec file with extra specs

--- spec/dealership_integration_spec.rb
+++ spec/dealership_integration_spec.rb
@@ -4,6 +4,9 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 describe('car dealership path', {:type => :feature}) do
+  before() do
+    Dealership.clear()
+  end
   it('processes the user entry and saves it') do
     visit('/dealerships/new')
     fill_in('name', :with => 'Bob')
@@ -11,6 +14,16 @@ describe('car dealership path', {:type => :feature}) do
     expect(page).to have_content('Success!')
   end
   it('processes the user entry and saves it') do
+    visit('/dealerships/new')
+    fill_in('name', :with => 'Rose')
+    click_button('Add Dealership')
+    click_link('See dealership List')
+    expect(page).to have_content('Dealerships Rose Return to Main Page')
+  end
+  it('processes the user entry and saves it') do
+    visit('/dealerships/new')
+    fill_in('name', :with => 'Rose')
+    click_button('Add Dealership')
     visit('/dealerships/1/vehicles/new')
     fill_in('make', :with => 'Toyota')
     fill_in('model', :with => 'Rav 4')
